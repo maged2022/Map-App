d6cd677d0bd394c3cb65c59761c9eaebeb85b9a8
maged mohamed <maged12345@Github.com>
2023-09-06 16:15:51 +0300

    Fixed the most of the map app code

---
+++ Map-App/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  Map-App
+//
+//  Created by s on 06/09/2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+

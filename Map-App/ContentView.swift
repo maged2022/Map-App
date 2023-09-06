9f8b358f2611e88168354be0fc263d648a40d4e5
maged mohamed <maged12345@Github.com>
2023-09-06 15:04:32 +0300

    Initial Commit

---
+++ Map-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Map-App
+//
+//  Created by s on 06/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Map-App/Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_AppApp.swift
+//  Map-App
+//
+//  Created by s on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
